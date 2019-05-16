330fb0205332970c1ee5a9a04f6353e8212b72b2
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-05-16 20:22:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,6 @@ function addElementToBeginningOfArray(chocolateBars,candyString) {
   return [candyString, ...chocolateBars];
 }
 function destructivelyAddElementToBeginningOfArray(chocolateBars,candyString) {
- chocolateBars.unshift(candyString);
- return chocolateBars;
+ chocolateBars.unshift(candyString)
+ return chocolateBars
 }
