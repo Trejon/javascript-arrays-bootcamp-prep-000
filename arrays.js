a603d11bded6530cb39fb569b068b12dec7d0fe0
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-05-16 21:57:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,6 +19,6 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
-  chocolateBare.shift();
+  chocolateBare.unshift();
   return chocolateBars;
 }
