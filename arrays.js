6907fbd10efd4beb71a62ff199aa077190e46b2e
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-05-16 20:11:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,4 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 function addElementToBeginningOfArray(chocolateBars,candyString) {
   return [candyString, ...chocolateBars];
 }
+function 
