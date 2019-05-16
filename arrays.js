2d4eacb0f5f2c97bfaf56175fb58a7ccd73951ff
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-05-16 20:58:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,6 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars,candyString) {
 function addElementToEndOfArray(chocolateBars,candyString) {
   return [...chocolateBars,candyString]
 }
+function addElementToEndOfArray(chocolateBars,candyString) {
+  choclateBars.push(candyString)
+}
